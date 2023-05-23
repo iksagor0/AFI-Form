c073dbfdb6d103a2a5c5f72994b971c39146425a
iksagor0 <iksagor0@gmail.com>
2023-05-23 19:18:02 +0600

    Social Security Number Pattern

--- Collector Vehicle Form/js/collector-vehicle-form.js
+++ Collector Vehicle Form/js/collector-vehicle-form.js
@@ -277,6 +277,18 @@ document
       : "(" + x[1] + ") " + x[2] + (x[3] ? "-" + x[3] : "");
   });
 
+// Social Security Number Pattern
+document.querySelectorAll(".SSN").forEach((field) => {
+  field.addEventListener("input", (e) => {
+    var x = e.target.value
+      .replace(/\D/g, "")
+      .match(/(\d{0,3})(\d{0,2})(\d{0,4})/);
+    e.target.value = !x[2]
+      ? x[1]
+      : x[1] + "-" + x[2] + (x[3] ? "-" + x[3] : "");
+  });
+});
+
 // Date Validation
 function dateValidation(field, getMaxYear) {
   field?.addEventListener("input", (e) => {
