6c8ad1f5068437e4f630b2959bf290817d562ee5
iksagor0 <iksagor0@gmail.com>
2023-07-11 19:30:13 +0600

    remove --Model, gitignore & index.html

--- --Model/Test/Collector-test.js
+++
@@ -1,8 +0,0 @@
-// Collector Vehicle-- Step 2 Form
-
-const appendTo = document.querySelector(".quote_request__summary");
-const cloneFrom = document.querySelector(".quote_request__summary_item");
-
-const clone = cloneFrom?.cloneNode(true);
-clone.querySelector(".quote_request__summary_item_info").innerText = "123";
-appendTo.appendChild(clone);
