edc46602f65d16e02934e9eaa1fc576d97e06710
iksagor0 <iksagor0@gmail.com>
2023-07-13 10:27:10 +0600

    make summaryHeading dynamic

--- Assets/collector-vehicle-form.js
+++ Assets/collector-vehicle-form.js
@@ -296,6 +296,11 @@ function runVehicleItemsFunctionality() {
 // *********************************************
 
 function summaryFunctionality() {
+  const summaryHeading = document.querySelector(
+    ".summary__form .quote_request_heading"
+  );
+
+  summaryHeading.innerHTML = `Your Policy Has ${collectorVehicles.length} Vehicles`;
   //
   // Check Main Vehicle data OKK or Not
   const mainVehicleFields = document.querySelectorAll(
